6dfeeeda9841dcfbf2481e345f5a6875ad389ab0
TheMaxMur <muravjev.mak@yandex.ru>
2023-04-12 02:24:01 +0300

    edit regex

--- dip/views/admin.py
+++ dip/views/admin.py
@@ -17,7 +17,7 @@ MAGIC_NUMBERS = { 'png': bytes([0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A])
                 }
 
 def validate_string(input_string: str) -> bool:
-    if (len(input_string) > 50) and (re.match("^[a-zA-Zа-яА-я0-9@.]+$", input_string)):
+    if (len(input_string) > 50) and (re.match("^[a-zA-Zа-яА-Я0-9@.]+$", input_string)):
         return False
     return True
 
